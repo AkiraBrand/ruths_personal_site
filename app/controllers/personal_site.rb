9e465be559f160c3103afd50d4c3fcb6bbacd136
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-01-18 17:07:53 -0700

    wip

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,8 +4,29 @@ class PersonalSite
 
   def self.call(env)
     # require 'pry'; binding.pryz
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
     ['200', {'Content-Type' => 'text/html'},
     [File.read('./app/views/index.html')]]
   end
 
+  def self.error
+    ['404', {'Content-Type' => 'text/html'},
+    [File.read('./app/views/error.html')]]
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'},
+    [File.read("./app/views/#{page}")]]
+  end 
 end
